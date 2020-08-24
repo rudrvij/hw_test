4473fe51f95bd4799ed8e3f25f178fd9371bc364
rudrvij <rudrapaka@gmail.com>
2020-08-24 13:35:57 -0500

    AnotherTest

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world by vijay11",
+            "message": "hello world by vijay112",
             # "location": ip.text.replace("\n", "")
         }),
     }
